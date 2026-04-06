0cfdbd042027cd11e6781d6728919a53fa397f73
Nicolas Kauan <nicolaskauanpro@gmail.com>
2026-04-06 09:45:50 -0300

    Criação do projeto

---
+++ src/main/java/com/nika/GerenciamentoDeClientes/GerenciamentoDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.nika.GerenciamentoDeClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeClientesApplication.class, args);
+	}
+
+}
